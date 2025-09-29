c4b23aeaf842cff734a8d8d5808657c60fccd695
KhanFahad1448 <fk9719650@gmail.com>
2025-09-29 18:35:41 +0530

    First commit

---
+++ src/Utils/new_collection.js
@@ -0,0 +1,48 @@
+let new_collections = [
+    {
+      id: 6,
+      name: "WHITE COTTON SHIRT",
+      category: "men",
+      image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTTPcFulD9lf9UbLANNhOvjfTWyolBvqJa7TdKEL3tT7yn69eyS3K3IqBHzh9zWE5tIKc8bHAt0PoJd3j4dIlaBPCijDx9PDLHTIAhFZpg',
+      image1: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRH9dLGTbem35sh32ad5YoAj2DdaT-p-dVvhpeov8RGh5llB9hG7Rk4QbZw4-zLFgVbHfc-MsCg1PFfb2efF7J7h7en3TFzqEG7Xq63",
+      image2: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQLtvR6-PelRlCFRZVlLK9cXKh7mEuDTnj4wbUw_L5FA-cXPT3NJrUba_pGFvzRsCCEQdowTWQ9ZOuaw70JzpmyIWllrTpei1vUFgJGjA',
+      image3: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSqQRWjTFzkyUMnXaPrCM9lXz9Yi9OX9C4SjFL3Pmdc6aW0HBcCwIAWXYvwBDGjnIXc7T31pXjHNtufRbWorwvMOgTiac_xYqRqRyUFwRVfAF5wakEMvRE',
+      new_price: 720.5,
+      old_price: 1285.0,
+    },
+    {
+      id: 9,
+      name: "Multi Color Floral Sweetheart Neck Mini Dress",
+      category: "women",
+      image: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/5/a5a0e2fTWN_TWDR710_1.jpg?rnd=20200526195200&tr=w-512',
+      image1: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/5/a5a0e2fTWN_TWDR710_2.jpg?rnd=20200526195200&tr=w-512',
+      image2: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/5/a5a0e2fTWN_TWDR710_4.jpg?rnd=20200526195200&tr=w-512',
+      image3: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/a/5/a5a0e2fTWN_TWDR710_5.jpg?rnd=20200526195200&tr=w-512',
+      new_price: 85.0,
+      old_price: 120.5,
+    },
+    {
+     id: 11,
+      name: "Olive Green Short Sleeves Shirt",
+      category: "men",
+      image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSBolA3qN3t7LSu7_QvAO8oausFenNNOOOtCkYdtD1JGuIaPUzV3lSy4bzAlGznJgBvoo0M3YL0B43OiNFq0GZvStDj6T0OgkD5tXEJstI',
+      image1: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcR8MZJRzltdmGWcxIjW0RqLlyoU8KGcv1CZk7RkzdVRk1w_5jZkv-E4-YfzDPeaFPErJZGifucMc8vj2VKJ-egR666JE4TWkyfFe3hM",
+      image2: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRUPVdICf3zSbh1ROeJf3uMuFXoMRCWmlFZTgn2hulez7e8OYBfmBAS4vBHTUGW1J0z9jhuICBJnbTkHMFwmsq958SwLpjZveLNMv5E",
+      image3: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQRzIjQH0Tr9iBVepg3CYBvZGpcSPiYniYlOAzo1QJHxenfr3ks-JfIYpdSumyAGb_KcOsJPKH7cWv4WvamnwxrcQ8WYLqNcsVDb8epHw",
+      new_price: 985.0,
+      old_price: 18120.5,
+    },
+    {
+      id: 4,
+      name: "Black Frill Around Me Dress",
+      category: "women",
+      image: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/4/e/4e9aa48TWN_TWENT00013335_1.jpg?rnd=20200526195200&tr=w-512',
+      image1: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/4/e/4e9aa48TWN_TWENT00013335_2.jpg?rnd=20200526195200&tr=w-512',
+      image2: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/4/e/4e9aa48TWN_TWENT00013335_4.jpg?rnd=20200526195200&tr=w-512',
+      image3: 'https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/4/e/4e9aa48TWN_TWENT00013335_5.jpg?rnd=20200526195200&tr=w-512',
+      new_price: 100.0,
+      old_price: 150.0,
+    } 
+    ];
+    
+    export default new_collections;
